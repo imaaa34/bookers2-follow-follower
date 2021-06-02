22314e872b18f240694f47cad93c934aaeb2f370
imaaa34 <oct.imd34@gmail.com>
2021-06-02 14:48:06 +0000

    [Add] users/infoページのみ表示成功

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,23 @@
+class RelationshipsController < ApplicationController
+ 
+  def create
+    @user = User.find(params[:relationship][:follower_id])
+    following = current_user.follower(@user)
+    if following.save
+      redirect_to @user
+    else
+      redirect_to @user
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:relationship][:follower_id])
+    following = current_user.unfollow(@user)
+    if following.destroy
+      redirect_to @user
+    else
+      redirect_to @user
+    end
+  end
+  
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,24 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+
+  has_many :followers, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
+  has_many :followeds, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
+  has_many :follower_users, through: :followed, source: :follower
+  has_many :followed_users, through: :follower, source: :followed
+
+  def follow(user_id)
+    follower.create(follower_id: user_id)
+  end
+
+  def unfollow(user_id)
+    follower.find_by(follower_id: user_id).destroy
+  end
+
+  def following?(user)
+    followed_users.include?(user)
+  end
+
   attachment :profile_image, destroy: false
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
